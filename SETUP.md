53538f0d59be29bed5e7b1b9f7c97c1db258a802
abhayp2204 <abhaypatil1020@gmail.com>
2022-05-09 12:41:21 +0530

    boilerplate

---
+++ SETUP.md
@@ -0,0 +1,4 @@
+1. npx create-react-app foo
+2. npm install firebase react-firebase-hooks
+3. cd foo
+4. npm start
---
+++ firechat/src/App.js
@@ -0,0 +1,11 @@
+import "./css/App.css"
+
+function App() {
+    return (
+        <div className="App">
+            Firechat
+        </div>
+    );
+}
+
+export default App
